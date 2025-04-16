af2cb5afc40b524bc6f67accdd9792b4a3e1961a
zimoluo <zimo_luo@outlook.com>
2025-04-16 18:11:39 -0400

    ahh

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import MasonryLayout from "@/components/ui/homepage/masonry-layout";
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   return (
     <main>
--- components/ui/homepage/masonry-layout.tsx
+++ components/ui/homepage/masonry-layout.tsx
@@ -1,8 +1,6 @@
 import { generateRandomPlaceholderImages } from "@/components/placeholder/placeholder-images";
 import Image from "next/image";
 
-export const dynamic = "force-dynamic";
-
 interface SmallCol {
   proportion: number;
 }
