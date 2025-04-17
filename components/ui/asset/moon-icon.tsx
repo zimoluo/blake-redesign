8cacffe8ce11bd7fc0d6f45af0ae2dd1d5a94adf
zimoluo <zimo_luo@outlook.com>
2025-04-17 19:07:45 -0400

    a smarter implementation

---
+++ components/ui/asset/moon-icon.tsx
@@ -0,0 +1,22 @@
+export default function MoonIcon({
+  className = "",
+  strokeClassName = "",
+}: AssetIcon) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      aria-label="Moon"
+      className={className}
+    >
+      <path
+        className={`${strokeClassName ? strokeClassName : "stroke-primary"}`}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={1.5}
+        d="M3.32 11.684a9 9 0 0 0 17.357 3.348A9 9 0 0 1 8.32 6.683c0-1.18.23-2.32.644-3.353a9.003 9.003 0 0 0-5.645 8.354Z"
+      />
+    </svg>
+  );
+}
--- components/ui/root-layout/light-dark-mode-toggle.tsx
+++ components/ui/root-layout/light-dark-mode-toggle.tsx
@@ -2,23 +2,43 @@
 
 import { useTheme } from "next-themes";
 import SunIcon from "../asset/sun-icon";
+import MoonIcon from "../asset/moon-icon";
+import { useEffect, useState } from "react";
 
 export default function LightDarkModeToggle() {
-  const { theme, setTheme } = useTheme();
+  const { theme, systemTheme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
 
-  const rotateThemes = () => {
-    if (theme === "dark") {
-      setTheme("system");
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted || !systemTheme) return <div className="w-6 h-6 invisible" />;
+
+  const isLight =
+    theme === "light" || (theme === "system" && systemTheme === "light");
+
+  const handleToggle = () => {
+    if (theme === "system") {
+      setTheme(systemTheme === "light" ? "dark" : "light");
     } else if (theme === "light") {
-      setTheme("dark");
-    } else {
-      setTheme("light");
+      systemTheme === "light" ? setTheme("dark") : setTheme("system");
+    } else if (theme === "dark") {
+      systemTheme === "dark" ? setTheme("light") : setTheme("system");
     }
   };
 
   return (
-    <button onClick={rotateThemes}>
-      <SunIcon className="w-6 h-6 transition-transform duration-300 ease-out hover:scale-110" />
+    <button
+      onClick={handleToggle}
+      aria-label="Toggle light/dark mode"
+      className="transition-transform duration-300 ease-out hover:scale-110"
+    >
+      {isLight ? (
+        <SunIcon className="w-6 h-6" />
+      ) : (
+        <MoonIcon className="w-6 h-6" />
+      )}
     </button>
   );
 }
