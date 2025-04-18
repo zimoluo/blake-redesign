f80d6ed27ad533438bd700bb7cddaa27b9aee69b
zimoluo <zimo_luo@outlook.com>
2025-04-17 20:08:20 -0400

    fuck it

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    rules: {
+      "@typescript-eslint/no-unused-expressions": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
