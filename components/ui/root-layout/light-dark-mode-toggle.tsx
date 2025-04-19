fb3e5d028e895e6ca1df1c8fa67b25d7412f1980
zimoluo <zimo_luo@outlook.com>
2025-04-19 13:04:37 -0400

    a smart, brilliant solution

--- components/ui/root-layout/light-dark-mode-toggle.tsx
+++ components/ui/root-layout/light-dark-mode-toggle.tsx
@@ -3,7 +3,6 @@
 import { useTheme } from "next-themes";
 import SunIcon from "../asset/sun-icon";
 import MoonIcon from "../asset/moon-icon";
-import { useEffect, useState } from "react";
 import toggleStyle from "./light-dark-mode-toggle.module.css";
 
 interface Props {
@@ -16,44 +15,33 @@ export default function LightDarkModeToggle({
   strokeClassName = "",
 }: Props) {
   const { theme, systemTheme, setTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted || !systemTheme) return <div className="w-6 h-6 invisible" />;
-
-  const isLight =
-    theme === "light" || (theme === "system" && systemTheme === "light");
 
   const handleToggle = () => {
     if (theme === "system") {
       setTheme(systemTheme === "light" ? "dark" : "light");
     } else if (theme === "light") {
       systemTheme === "light" ? setTheme("dark") : setTheme("system");
-    } else if (theme === "dark") {
+    } else {
       systemTheme === "dark" ? setTheme("light") : setTheme("system");
     }
   };
 
   return (
     <button
-      onClick={handleToggle}
       aria-label="Toggle light/dark mode"
-      className={`w-6 h-6 ${className}`}
+      onClick={handleToggle}
+      className={`relative w-6 h-6 group ${className}`}
     >
-      {isLight ? (
-        <SunIcon
-          className={`w-6 h-6 transition-transform duration-300 ease-out hover:scale-110 ${toggleStyle.startup}`}
-          strokeClassName={strokeClassName}
-        />
-      ) : (
-        <MoonIcon
-          className={`w-6 h-6 transition-transform duration-300 ease-out hover:scale-110 ${toggleStyle.startup}`}
-          strokeClassName={strokeClassName}
-        />
-      )}
+      <SunIcon
+        aria-hidden
+        className={`icon-sun absolute inset-0 transition-transform duration-300 group-hover:scale-110 ${toggleStyle.sun}`}
+        strokeClassName={strokeClassName}
+      />
+      <MoonIcon
+        aria-hidden
+        className={`icon-moon absolute inset-0 transition-transform duration-300 group-hover:scale-110 ${toggleStyle.moon}`}
+        strokeClassName={strokeClassName}
+      />
     </button>
   );
 }
