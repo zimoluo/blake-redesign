79567505c39dcbfe0c51aea0818b3f1b8fe548a5
zimoluo <zimo_luo@outlook.com>
2025-04-25 13:56:20 -0400

    remove the special border radius

--- app/lightbox/layout.tsx
+++ app/lightbox/layout.tsx
@@ -12,19 +12,19 @@ export default function LightboxLayout() {
           className={`grid ${containerStyle.mobileGrid} md:flex h-full w-full p-3 border-reflect ${containerStyle.borderContainer} ${containerStyle.bgContainer} shadow-xl/5 rounded-3xl text-dark`}
         >
           <div
-            className={`rounded-[1rem_1rem_0.65rem_0.65rem] md:rounded-[1rem_0.65rem_0.65rem_1rem] ${containerStyle.bg} ${containerStyle.borderAdjust} h-full md:mr-3 flex-grow border-reflect overflow-hidden p-px`}
+            className={`rounded-2xl ${containerStyle.bg} ${containerStyle.borderAdjust} h-full md:mr-3 flex-grow border-reflect overflow-hidden p-px`}
           >
             <LightboxCanvas />
           </div>
           <SidebarWrapper>
             <div
-              className={`rounded-[0.65rem_0.65rem_0.65rem_1rem] md:rounded-[0.65rem] ${containerStyle.bg} ${containerStyle.borderAdjust} w-full md:w-48 h-full border-reflect`}
+              className={`rounded-2xl ${containerStyle.bg} ${containerStyle.borderAdjust} w-full md:w-48 h-full border-reflect`}
             >
               this will hold the layers
             </div>
           </SidebarWrapper>
           <div
-            className={`rounded-[0.65rem_0.65rem_1rem_0.65rem] md:rounded-[0.65rem_1rem_1rem_0.65rem] ${containerStyle.bg} ${containerStyle.borderAdjust} h-full w-12 border-reflect`}
+            className={`rounded-2xl ${containerStyle.bg} ${containerStyle.borderAdjust} h-full w-12 border-reflect`}
           >
             <div
               className={`w-full h-full flex flex-col items-center ${containerStyle.buttonContainer} overflow-x-hidden overflow-y-auto py-2 gap-2`}
